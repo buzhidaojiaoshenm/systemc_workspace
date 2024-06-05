1143681aa97e68b19ee0e1a38c69b01082278937
sjh <2651142905@qq.com>
2024-06-05 20:12:46 +0800

    add README

---
+++ README.md
@@ -0,0 +1,5 @@
+compiler and run systemc code 
+
+how to use：
+1. modify path in compiler shell script
+2. ./compiler.sh ./hello/main
--- compiler.sh
+++ compiler.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 target=$1
 
+include_path=/home/sjh/work/systemc-3.0.0/include
+lib_path=/home/sjh/work/systemc-3.0.0/lib-linux64
+
 export SYSTEMC_HOME=/home/sjh/work/systemc-3.0.0
 export LD_LIBRARY_PATH=/home/sjh/work/systemc-3.0.0/lib-linux64:$LD_LIBRARY_PATH
 
-g++ -o $target $target.cpp -I/home/sjh/work/systemc-3.0.0/include -L/home/sjh/work/systemc-3.0.0/lib-linux64 -lsystemc
+g++ -o $target $target.cpp -I$include_path -L$lib_path -lsystemc
 ./$target
